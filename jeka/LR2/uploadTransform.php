45d7b1d30b9e5a31d659554efe786de757a1ec47
typaYa <95447218+typaYa@users.noreply.github.com>
2023-09-20 17:47:42 +0300

    Update uploadTransform.php

--- jeka/LR2/uploadTransform.php
+++ jeka/LR2/uploadTransform.php
@@ -5,45 +5,43 @@ $dir = "img";
 if(!is_dir($dir)) {
     mkdir($dir, 0777, true);
 }
-if (isset($_POST['upload'])){
+if (isset($_POST['upload'])) {
     echo 'qwe';
-    $connect = mysqli_connect('localhost','root','','jeka');
-    if (!empty($_FILES['file'])){
+    $connect = mysqli_connect('localhost', 'root', '', 'lr2');
+    if (!empty($_FILES['file'])) {
 
         $file = $_FILES['file'];
-        $name = $file['name'];
-        $pathFile = __DIR__ . '/img/' .$name;
+        $pathFile = $file['name'];
 
-        if (!move_uploaded_file($file['tmp_name'], $pathFile)){
+        if (!move_uploaded_file($file['tmp_name'], $pathFile)) {
             echo 'Что-то не так';
-        }else {
+        } else {
             $post = $_POST['post'];
             $desc = $_POST['desc'];
             $cost = $_POST['cost'];
-            $name2 = $_POST['name'];
+            $name = $_POST['name'];
 
-            $selectMaxId = mysqli_query($connect, "select MAX(id) from `img`");
+            $selectMaxId = mysqli_query($connect, "select MAX(id) from `facture`");
             $fetchId = mysqli_fetch_assoc($selectMaxId);
             $id = $fetchId['MAX(id)'] + 1;
-            mysqli_query($connect, "INSERT INTO `img`(`id`,`name`,`path`,`description`,`cost`,`post`)values ('$id','$name2','$name','$desc','$cost','$post')");
 
-            mysqli_query($connect, "select `name` from `postavshik` where `name`='$post'");
+            mysqli_query($connect, "select `name` from `reg` where `name`='$post'");
             echo mysqli_affected_rows($connect);
             if (mysqli_affected_rows($connect) >= 1) {
-                $selectIdPost = mysqli_query($connect,"select `id` from `postavshik` where `name` = '$post'");
+                $selectIdPost = mysqli_query($connect, "select `id` from `reg` where `name` = '$post'");
                 $fetchId = mysqli_fetch_assoc($selectIdPost);
-                $id1 = $fetchId['id'];
-                echo $id1;
-                mysqli_query($connect, "INSERT INTO `postavshik`(`id`,`name`,`id_tovara`)values('$id1','$post','$id')");
-            }else{
-                $selectMaxId = mysqli_query($connect, "select MAX(id) from `postavshik`");
+                $regId = $fetchId['id'];
+                $id_region = mysqli_fetch_assoc($selectIdPost);
+                echo $regId;
+                mysqli_query($connect, "INSERT INTO `facture`(`id`,`name`,`id_region`,`description`,`money`,`path`)values('$id','$name','$regId','$desc','$cost','$pathFile')");
+            } else {
+                $selectMaxId = mysqli_query($connect, "select MAX(id) from `reg`");
                 $fetchId = mysqli_fetch_assoc($selectMaxId);
-                $id2 = $fetchId['MAX(id)'] + 1;
-                mysqli_query($connect, "INSERT INTO `postavshik`(`id`,`name`,`id_tovara`)values('$id2','$post','$id')");
+                $regId = $fetchId['MAX(id)'] + 1;
+                mysqli_query($connect, "INSERT INTO `reg`(`id`,`name`)values('$regId','$post')");
+                mysqli_query($connect, "INSERT INTO `facture`(`id`,`name`,`id_region`,`description`,`money`,`path`)values('$id','$name','$regId','$desc','$cost','$pathFile')");
             }
+            header('Location:/lr2/cait.php');
         }
-
-        header('Location:/addTransfrom.php');
     }
 }
-
